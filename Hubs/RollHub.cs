1644c9490bea55c397d5b760b665fbc340850bd0
Pascal Paradis <pascal.paradis@gmail.com>
2014-08-31 21:51:50 -0400

    Ajustements

--- Hubs/RollHub.cs
+++ Hubs/RollHub.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNet.SignalR;
+using System;
 using System.Threading.Tasks;
-using System.Web;
-using Microsoft.AspNet.SignalR;
 
 namespace Slashroll.Hubs
 {
